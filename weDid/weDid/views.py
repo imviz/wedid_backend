69821f50d2845f96771f1506206f77de3f0c38fb
imviz <vishnuedappatt@gmail.com>
2022-10-07 09:59:09 +0530

    changes made

--- weDid/weDid/views.py
+++ weDid/weDid/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-# from django.views.decorators.cache import never_cache
+from django.views.decorators.cache import never_cache
 from django.conf import settings
 
 def index(request):
