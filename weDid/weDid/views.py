0c4698ba432c38652f3680a54777e921e0a5fdf9
imviz <vishnuedappatt@gmail.com>
2022-10-07 10:55:39 +0530

    function changes

--- weDid/weDid/views.py
+++ weDid/weDid/views.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 from django.conf import settings
 
-def index(request):
-    return render(request,'index.html')
+
+index=never_cache(TemplateView.as_view(template_name='index.html'))
